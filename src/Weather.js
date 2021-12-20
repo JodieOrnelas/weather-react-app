212caf23d7fae6b033186fcdc43df90130916854
Jodie <88211843+JodieOrnelas@users.noreply.github.com>
2021-12-20 14:16:09 -0600

    Added basic styling

---
+++ src/Weather.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Wheather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          {" "}
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3 ">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+
+      <h1>Houston</h1>
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          <span className="temperature"> 6</span>
+          <span className="unit">ºC</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Percipitation: 15%</li>
+            <li>Humidity:72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
