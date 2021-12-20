ec4cb0a26b78e91087844e6839a494c78ab8f4cb
Jodie <88211843+JodieOrnelas@users.noreply.github.com>
2021-12-20 12:15:20 -0600

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a href="https://fervent-roentgen-43ed2b.netlify.app/">Jodie Ornelas</a>{" "}
+        and is open sourced on{" "}
+        <a href="https://github.com/JodieOrnelas/weather-react-app">GitHub</a>
+      </footer>
     </div>
   );
 }
