6e510dce597a444045ab47caa1ab021740122f55
Jodie <88211843+JodieOrnelas@users.noreply.github.com>
2021-12-20 13:31:03 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <div className="container">
+        <h1>Weather app</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a href="https://fervent-roentgen-43ed2b.netlify.app/">Jodie Ornelas</a>{" "}
-        and is open sourced on{" "}
-        <a href="https://github.com/JodieOrnelas/weather-react-app">GitHub</a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://fervent-roentgen-43ed2b.netlify.app/">
+            Jodie Ornelas
+          </a>{" "}
+          and is open sourced on{" "}
+          <a href="https://github.com/JodieOrnelas/weather-react-app">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
